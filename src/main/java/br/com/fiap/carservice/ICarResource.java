236687e177f05308808800679579fe3b37cd1300
Rafael Petronilio <rafaelpetronilio@gmail.com>
2019-05-15 23:01:32 -0300

    Primeiro teste mocado construido

--- src/main/java/br/com/fiap/carservice/ICarResource.java
+++ src/main/java/br/com/fiap/carservice/ICarResource.java
@@ -1,4 +1,12 @@
 package br.com.fiap.carservice;
 
-public interface ICarResource {
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+@RepositoryRestResource(path = "cars",collectionResourceRel = "cars")
+public interface ICarResource extends PagingAndSortingRepository<Car,Integer> {
+    List<Car> findCarByColor(@Param("color") String color);
 }
--- src/test/java/br/com/fiap/carservice/CarResourceMockTest.java
+++ src/test/java/br/com/fiap/carservice/CarResourceMockTest.java
@@ -3,7 +3,12 @@ package br.com.fiap.carservice;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CarResourceMockTest {
@@ -29,6 +34,24 @@ public class CarResourceMockTest {
         car.setModel(model);
         car.setColor(color);
 
-        carResource.findCarByColor(car);
+        List<Car> cars = Arrays.asList(car);
+
+        //TDD
+        //   When *
+        //   Then * mocado
+        //   Assert
+        //quando chamar o metodo findCarByColor então retorne a lista de carros
+        Mockito.when(carResource.findCarByColor(color))
+               .thenReturn(cars);
+
+        //executar
+        List<Car> carsResponse = carResource.findCarByColor(color);
+
+        //TDD
+        //   When
+        //   Then
+        //   Assert *
+        assertEquals(carsResponse.get(0).getColor(),color);
+        assertEquals(carsResponse.get(0).getModel(),model);
     }
  }
