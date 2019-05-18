131f6d78c5fe283659c4f2a4b48c968b63d21085
Rafael Petronilio <rafaelpetronilio@gmail.com>
2019-05-18 15:29:30 -0300

    Aplicação customer

--- customer/src/main/java/br/com/fiap/customer/entity/Customer.java
+++ customer/src/main/java/br/com/fiap/customer/entity/Customer.java
@@ -4,10 +4,15 @@ package br.com.fiap.customer.entity;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Getter
 @Setter
+@Entity
 public class Customer {
 
+    @Id
     private Integer id;
     private String name;
     private String lastName;
