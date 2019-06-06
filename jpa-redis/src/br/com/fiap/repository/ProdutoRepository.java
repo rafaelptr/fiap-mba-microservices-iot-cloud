624f4e0393d2b7d157b4736bee71bd1c60ebebf2
Rafael Petronilio <rafaelpetronilio@gmail.com>
2019-06-05 21:41:29 -0300

    Aula de redis 05/06

---
+++ jpa-redis/src/br/com/fiap/repository/ProdutoRepository.java
@@ -0,0 +1,4 @@
+package br.com.fiap.repository;
+import org.springframework.data.repository.CrudRepository;
+import br.com.fiap.entity.Produto;
+public interface ProdutoRepository extends CrudRepository<Produto, Long> {} 
